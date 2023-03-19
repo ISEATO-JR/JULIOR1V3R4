d2e3d2659d28faf76a7177d0a74846cc7fe8609d
ISEATO-JR <julio.rivera.1596@gmail.com>
2023-03-18 23:47:36 -0600

    Terminando ver lo de funciones basico

--- Test-jr05/js/app-20.js
+++ Test-jr05/js/app-20.js
@@ -1,1 +1,39 @@
-// Metodo con propiedad
+// Funcion con Metodo de propiedad
+const reproductor = {
+    reproducir : function(id) {
+        console.log(`Reproduciendo Cancion: ${id}`);
+    },
+
+    pausar : function(){
+        console.log(`Pausando...`);
+    },
+
+    crearPlayList : function(nombre) {
+        console.log(`Creando el playlist: ${nombre}`);
+    },
+
+    reproducirPlayList : function(nombre) {
+        console.log(`Reproducioendo el playlist: ${nombre}`);
+    }
+}
+
+// Declarando una funcion de eliminar
+reproductor.borrarCancion = function(id){
+    console.log(`Eliminando la cancion: ${id}`);
+
+}
+// Invocando funcion y ejecuntando 
+console.log(reproductor);
+
+// Accediendo en la propiedad de la funcion
+reproductor.reproducir(5656);
+reproductor.pausar();
+
+// invocando funcion de borrarCancion();
+reproductor.borrarCancion(56);
+
+// invocando la funcion de crear playList();
+reproductor.crearPlayList('Enamorado');
+
+//Invocando la funcion de reproducirPlaylist();
+reproductor.reproducirPlayList(`Enamorado`);
