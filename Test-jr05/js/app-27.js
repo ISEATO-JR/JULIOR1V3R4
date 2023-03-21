871c1546506df677ec4bc19864cf14d22012759e
ISEATO-JR <julio.rivera.1596@gmail.com>
2023-03-20 19:55:15 -0600

    loop y POO

---
+++ Test-jr05/js/app-27.js
@@ -0,0 +1,14 @@
+// Programacion orientada a objeto
+
+// Ejemplo 01
+// Object Literal
+const producto = {
+    nombre: 'tablet',
+    precio : 500
+}
+
+// Ejemplo 02
+// Objeto Constructor
+function Producto() {
+    
+}
