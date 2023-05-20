2473b4949fb47488ee239d37e2067613214eeb9d
ISEATO-JR <julio.rivera.1596@gmail.com>
2023-05-19 23:25:36 -0600

    Validacionde fomulario en campos vacios y datos

--- Test-jr06/js/app01.js
+++ Test-jr06/js/app01.js
@@ -1,25 +1,103 @@
 // Evento en JavaScript
 
-console.log(1);
+// console.log(1);
 
 window.addEventListener('load',function(){ // load espera a que el JS  y los archivos  que dependan  del HTML  esten listo
-    console.log(2);    
+    // console.log(2);    
 })
 
 window.onload = function(){
-    console.log(3);
+    // console.log(3);
     
 }
 
 document.addEventListener('DOMContentLoaded', function(){ // Solo espera  por el HTML, pero no espera CSS o imagenes
-    console.log(4);
+    // console.log(4);
     
 })
 
-console.log(5);
+// console.log(5);
 
 //Evento scrolling
 document.onscroll = function(){
-    console.log('scrolling...!');
-    
+    // console.log('scrolling...!');
 }
+
+// Seleccionar elementos y asociarles un evento
+const btnEnviar = document.querySelector('.boton--primario');
+btnEnviar.addEventListener('Click', function(evento){
+    console.log(evento);
+    evento.preventDefault();
+    console.log('Enviando formulario');
+});
+
+// Evento de los Inputs y Textarea
+
+const datos = {
+    nombre: '',
+    email: '',
+    mensaje: ''
+}
+
+const nombre = document.querySelector('#nombre'); 
+const email = document.querySelector('#email'); 
+const mensaje = document.querySelector('#mensaje'); 
+const formulario = document.querySelector('.formulario');
+// input
+// change
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+// El evento de Submit
+formulario.addEventListener('submit',function(evento){
+evento.preventDefault();
+
+// Validacionde formulario
+const {nombre,email, mensaje} = datos;
+if(nombre === '' || email=== '' || mensaje === ''){
+    mostrarError('Todo los campos son obligatorios');
+
+    return; // Corta la ejecucion del codigo
+}
+
+// Enviar la alerta de Enviar Correctamenete
+mostrarMensaje('Mensaje enviando correctamente');
+
+// console.log('Enviando formularios');
+});
+
+function  leerTexto(e){
+   // console.log(e.target.vale);
+    // console.log(e.target);
+    datos[e.target.id] = e.target.value;
+    // console.log(datos)
+}
+
+// Muestra una alerta correctamente
+function mostrarMensaje(mensaje){
+    const alerta = document.createElement('P');
+    alerta.textContent = mensaje;
+    alerta.classList.add('correcto');
+    formulario.appendChild(alerta);
+
+    setTimeout(()=>{
+        alerta.remove()
+    },2000);
+}
+
+function mostrarError(mensaje){
+    const error = document.createElement('P');
+    error.textContent= mensaje;
+    error.classList.add('error');
+
+    // console.log(error);
+    formulario.appendChild(error);
+
+    // Desaparezaca el texto
+    setTimeout(() =>{
+        error.remove();
+
+    },2000);
+}
+
--- Test-jr06/js/app02.js
+++ Test-jr06/js/app02.js
@@ -3,7 +3,5 @@ const btnEnviar = document.querySelector('.boton--primario');
 btnEnviar.addEventListener('click', function(evento){
     console.log(evento);
     console.log(evento.preventDefault());
-
     console.log('Enviando Formulario');
-    
 });
